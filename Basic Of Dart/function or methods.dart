1731f7f497bf30c5703babaa2ef7932a74d891f9
Avinash175 <avinashmagar15@gmail.com>
2022-11-12 09:55:42 +0530

    Function with Parameter

--- Basic Of Dart/function or methods.dart
+++ Basic Of Dart/function or methods.dart
@@ -1,16 +1,24 @@
 void main(){
   //function
-  add();
+  add(4,5);
 
 
 }
 
 //finction declaration
-
+/*
 void add(){
   int a=5,b=6,c;
   c=a+b;
   print("$c is the addition of given number");
+
+ */
+
+void add(int a,int b){
+
+  int c=a+b;
+  print("$c is addition of two number");
+
 
 
 }
