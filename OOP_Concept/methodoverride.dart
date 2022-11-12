5533173247268796c44c7a093d05cbcf84273563
Avinash175 <avinashmagar15@gmail.com>
2022-11-12 17:46:35 +0530

    method overridde

--- OOP_Concept/methodoverride.dart
+++ OOP_Concept/methodoverride.dart
@@ -1,4 +1,34 @@
 void main()
 {
-  
+  Peoples peoples=Peoples();
+  Man man=Man();
+  women w=women();
+  w.identity();
+  w.i();
+  w.id();
+
 }
+
+class Peoples{
+
+  void identity(){
+    print("We Are all One");
+  }
+
+}
+class Man extends Peoples{
+
+  void id(){
+    print("We are men");
+  }
+
+}
+class women extends Man{
+
+  void i(){
+    print("we are women");
+    super.id();
+  }
+
+}
+
