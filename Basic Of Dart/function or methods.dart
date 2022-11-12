bff6def9e8bd0b51b73418629323b6638d84d2a2
Avinash175 <avinashmagar15@gmail.com>
2022-11-12 10:10:41 +0530

    Short Hand Thechnique

--- Basic Of Dart/function or methods.dart
+++ Basic Of Dart/function or methods.dart
@@ -1,6 +1,7 @@
 void main(){
   //function
   add(4,5);
+  mul(3, 2);
 
 
 }
@@ -19,6 +20,13 @@ void add(int a,int b){
   int c=a+b;
   print("$c is addition of two number");
 
-
-
 }
+
+
+int mul(int c,int d){
+
+  int mul=c*d;
+  print("$mul is the final result");
+   return mul;
+}
+
---
+++ Basic Of Dart/short_hand.dart
@@ -0,0 +1,5 @@
+void main(){
+add(2, 2);
+}
+
+void add(int a,int b) =>print("The Addition Of Two Number Is ${(a+b)} ");
