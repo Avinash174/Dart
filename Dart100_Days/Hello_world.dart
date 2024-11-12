99394520c7def7afa0cd9db112f745e8a4f3b3a4
Avinash Magar <42104652+Avinash174@users.noreply.github.com>
2024-11-12 09:21:06 +0530

    new  code added

---
+++ Dart100_Days/Hello_world.dart
@@ -0,0 +1,13 @@
+void main() {
+  print("Hello All"); // print statement use to print the anything on the screen
+}
+
+  // From void main program start the execution.
+
+  // void is the return type that not return anything.
+
+  // For Comments
+
+  // Single Line Comments 
+
+  /* For Multi-Line Comment */
