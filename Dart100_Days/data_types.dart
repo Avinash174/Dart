f3b98d854ed67f871e512fa2fcc3c3d88d2bb2c8
Avinash Magar <42104652+Avinash174@users.noreply.github.com>
2024-11-12 09:29:28 +0530

    new code

---
+++ Dart100_Days/data_types.dart
@@ -0,0 +1,44 @@
+/* 
+
+Data Types In Dart
+
+1.Number
+
+2.String 
+
+3.booleans
+
+4.List
+
+5.sets
+
+6.maps
+
+7.runes
+
+8.symbols
+*/
+
+/*
+ Number Data Types
+
+ 1. Int
+
+ 2. Double
+ 
+*/
+
+void main() {
+  int a = 10;
+
+  print(a);
+
+  double b = 5.0;
+  print(b);
+
+  String name = "Avinash";
+  print(name);
+
+  bool h = true;
+  print(h);
+}
