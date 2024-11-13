3ec33919a703e21a2564798583bb76658859e8e6
Avinash Magar <42104652+Avinash174@users.noreply.github.com>
2024-11-13 09:04:39 +0530

    string concept

---
+++ Dart100_Days/string_interpolation.dart
@@ -0,0 +1,20 @@
+/* There are two type of string interpolation 
+
+1. Single cot (' ')
+
+2. Double Cot (" ")
+
+*/
+
+void main(){
+
+  // Single Cot Example
+
+  String name='Avinash Magar';
+  print(name);
+
+  // Double Cot Example
+
+  String myName="Avinash's Program";
+  print(myName);
+}
