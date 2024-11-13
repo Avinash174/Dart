19eceb395afbf9113862d464b84a2c63dc3168cf
Avinash Magar <42104652+Avinash174@users.noreply.github.com>
2024-11-13 09:32:39 +0530

    new code

---
+++ Dart100_Days/var_key.dart
@@ -0,0 +1,19 @@
+// Var keyword
+
+void main(){
+  // var keyword 
+  var a='avinash';
+  print("Data Type Is ${a.runtimeType}");
+
+
+  // Same data type can change in var 
+  var b=true;
+  print(b);
+  b=false;
+  print(b);
+
+
+
+
+
+}
