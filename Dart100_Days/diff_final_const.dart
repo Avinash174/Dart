5412b4dc97356c458c933d654708d72e08c20ab5
Avinash Magar <42104652+Avinash174@users.noreply.github.com>
2024-11-15 09:47:05 +0530

    new code

---
+++ Dart100_Days/diff_final_const.dart
@@ -0,0 +1,16 @@
+/*
+Differnce Between final and const 
+
+final 
+
+1. final keyword set once but initialize when it is accessed.
+
+2. Instance varible can be final but cannot be const  
+
+const 
+
+1. It is compilation time constant 
+
+
+*/
+
