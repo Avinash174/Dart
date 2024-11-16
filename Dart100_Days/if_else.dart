68ff33ade315eb7b5eced9803913a6d881998d47
Avinash Magar <42104652+Avinash174@users.noreply.github.com>
2024-11-16 09:28:53 +0530

    new changes done

--- Dart100_Days/if_else.dart
+++ Dart100_Days/if_else.dart
@@ -8,15 +8,44 @@ statement;
 }
 else{
 statement ;
+
+Nested If-Else
+
+if(exp){
+statement;
+
+}else if(exp){
+statement;
+}else if(exp){
+state;
+}else{
+statement 
+}
 }
 */
 
 void main() {
   var a = 10;
 
+  // if statement
+
   if (a == 10) {
     print(true);
   } else {
     print(false);
+  }
+
+  //nested if-else
+
+  var b = 10;
+
+  if (b == 10) {
+    print("Enter Number Is equal to : $b");
+  } else if (b < 10) {
+    print("$b is greather");
+  } else if (b > 10) {
+    print("$b is smaller");
+  } else {
+    print("$b is nothing");
   }
 }
