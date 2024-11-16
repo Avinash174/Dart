e4f2d3fcad66e34ada3fdfb92c7bccd4876c9e6b
Avinash Magar <42104652+Avinash174@users.noreply.github.com>
2024-11-16 09:16:17 +0530

    new changes done

---
+++ Dart100_Days/if_else.dart
@@ -0,0 +1,22 @@
+// if-else statement
+
+/*
+syntax :
+
+if(test exp){
+statement;
+}
+else{
+statement ;
+}
+*/
+
+void main() {
+  var a = 10;
+
+  if (a == 10) {
+    print(true);
+  } else {
+    print(false);
+  }
+}
