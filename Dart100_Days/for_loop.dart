7263ae30e3149c52c2bb8de88858b136299d4e67
Avinash Magar <42104652+Avinash174@users.noreply.github.com>
2024-11-16 09:44:46 +0530

    new code for loop

--- Dart100_Days/for_loop.dart
+++ Dart100_Days/for_loop.dart
@@ -1,7 +1,19 @@
 // for loop
 
 void main() {
+  //for loop
+
   for (var i = 1; i <= 5; i++) {
     print("$i");
+
+    // nested for loop
+
+    for (var j = 0; j < 5; j++) {
+      print("The Value Of i=$i");
+      for (var k = 0; k < 3; k++) {
+        print("The Value Of j=$j");
+      }
+      print(" ");
+    }
   }
 }
