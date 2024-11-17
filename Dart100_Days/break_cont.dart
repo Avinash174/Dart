4ffb25726ba62ec9a2cc8c0ffa35f1824f09f406
Avinash Magar <42104652+Avinash174@users.noreply.github.com>
2024-11-17 20:55:40 +0530

    switch_case

--- Dart100_Days/break_cont.dart
+++ Dart100_Days/break_cont.dart
@@ -1,8 +1,18 @@
 void main() {
-  for (var i = 1; i < 10; i++) {
+  // break
+  for (var i = 0; i < 5; i++) {
     if (i == 2) {
       break;
     }
     print("i : $i");
+  }
+  print(" ");
+  // continue
+
+  for (var j = 0; j < 5; j++) {
+    if (j == 2) {
+      continue;
+    }
+    print("j : $j");
   }
 }
---
+++ Dart100_Days/switch_case.dart
@@ -0,0 +1,17 @@
+void main() {
+  var a = 10;
+
+  switch (a) {
+    case 1:
+      print("Case Valid");
+      break;
+    case 2:
+      print("case 2");
+      break;
+    case 3:
+      print("case 3");
+      break;
+    default:
+      print("Invalid Option");
+  }
+}
