67a38af2f470c79126a11278f596df0cd517227b
Avinash Magar <42104652+Avinash174@users.noreply.github.com>
2024-11-17 20:44:49 +0530

    new code

---
+++ Dart100_Days/while_do.dart
@@ -0,0 +1,16 @@
+// while vs do-while loop diffrence
+
+void main() {
+  print("While Loop:\n");
+  int i = 1;
+  while (i < 5) {
+    print(i);
+    i++;
+  }
+  print("Do-While Loop");
+  int j = 0;
+  do {
+    print(i);
+    i++;
+  } while (j <= 5);
+}
