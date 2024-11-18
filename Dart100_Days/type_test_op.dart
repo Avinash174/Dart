907fd2bfae27377fb11239d3c89a1af4cf34b473
Avinash Magar <42104652+Avinash174@users.noreply.github.com>
2024-11-18 09:35:09 +0530

    is & is! op

--- Dart100_Days/type_test_op.dart
+++ Dart100_Days/type_test_op.dart
@@ -1,1 +1,17 @@
+/*
+Type-Test Operator 
+1. is 
 
+if the object has the specified type then it is true.
+
+2. is !
+
+If the object doesn't have the specified type then it true.
+
+*/
+
+void main() {
+  var a = 'hello';
+  print(a is double);
+  print(a is! int);
+}
