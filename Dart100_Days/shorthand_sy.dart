33764854a42c2e37dabcd68227a0343ab86faea5
Avinash Magar <42104652+Avinash174@users.noreply.github.com>
2024-11-20 08:55:47 +0530

    short_hand tech

--- Dart100_Days/shorthand_sy.dart
+++ Dart100_Days/shorthand_sy.dart
@@ -1,1 +1,20 @@
-// Short hand 
+// Short hand or Fat Arrow Technique
+
+/* 
+For function that contain just one expression 
+you can use a shorthand syntax.
+
+The arrow => notation is sometimes referred to
+as arrow syntax.
+*/
+
+import 'finction.dart';
+
+void main() {
+  print(add(10, 20));
+  print(sub(10, 9));
+}
+
+int add(int a, int b) => a + b;
+
+int sub(int b, int c) => b - c;
