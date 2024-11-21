70701a03d9a4d9965b78a893eb00a4129301f70e
Avinash Magar <42104652+Avinash174@users.noreply.github.com>
2024-11-21 18:11:26 +0530

    new code

--- Dart100_Days/lexical_clauser.dart
+++ Dart100_Days/lexical_clauser.dart
@@ -8,20 +8,20 @@ variable in its lexical variable in its
 lexical scope even when the function is
 used outside of its original scope.
  */
- var a=0;
- void main(){
- 
+var a = 0;
+void main() {
   print('main fun = $a');
 
-  void outer(){
-    a=5;
+  void outer() {
+    a = 5;
     print('Outer fun = $a');
   }
+
   outer();
   Demo();
- }
+}
 
- void Demo(){
-  a=20;
+void Demo() {
+  a = 20;
   print('Demo func = $a');
- }
+}
