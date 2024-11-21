c88fe408632243e97b43e16ed1a40fe216f5f8e2
Avinash Magar <42104652+Avinash174@users.noreply.github.com>
2024-11-21 09:47:13 +0530

    new code

--- Dart100_Days/returnfu.dart
+++ Dart100_Days/returnfu.dart
@@ -1,3 +1,15 @@
 /*
 Return A Function From Another Function
 */
+
+void main() {
+  var x = marks();
+  print(x(10, 20, 30));
+}
+
+Function marks() {
+  Function num = (int a, int b, int c) {
+    return a + b + c;
+  };
+  return num;
+}
