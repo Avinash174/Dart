b9fe9fa424dd5df876f96806eb85961ca268da26
Avinash Magar <42104652+Avinash174@users.noreply.github.com>
2024-11-21 18:22:07 +0530

    new changes done

---
+++ Dart100_Days/list.dart
@@ -0,0 +1,23 @@
+/* 
+
+List is similar to array.
+
+In dart, arrays are list objects and elements 
+are ordered in sequence in a LIST.
+
+Zero-Base Indexing Like Array
+
+  1. Fixed List 
+  2. Growable List
+
+*/
+
+// List Declaration
+
+void main() {
+  List<int> lst = [10, 20, 30, 40];
+  List<String> lst1 = ['avi', 'nn'];
+  print(lst);
+  print(lst1.runtimeType);
+  print(lst1[1]);
+}
