06a415cbc26a54a0f84e21c7d197717dd82dd5bf
Avinash Magar <42104652+Avinash174@users.noreply.github.com>
2024-11-21 18:10:45 +0530

    lexical clouser

--- Dart100_Days/lexical_clauser.dart
+++ Dart100_Days/lexical_clauser.dart
@@ -1,5 +1,27 @@
 /*
 Lexical Clouseres
 
+Clouser 
 
+Is Function object that has access to 
+variable in its lexical variable in its
+lexical scope even when the function is
+used outside of its original scope.
  */
+ var a=0;
+ void main(){
+ 
+  print('main fun = $a');
+
+  void outer(){
+    a=5;
+    print('Outer fun = $a');
+  }
+  outer();
+  Demo();
+ }
+
+ void Demo(){
+  a=20;
+  print('Demo func = $a');
+ }
