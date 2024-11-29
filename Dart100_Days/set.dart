32a0296100157657851fd6e16849b84eeac74043
Avinash Magar <42104652+Avinash174@users.noreply.github.com>
2024-11-29 08:42:52 +0530

    set part one

---
+++ Dart100_Days/set.dart
@@ -0,0 +1,19 @@
+/* 
+
+set 
+
+A set in a dart is an unordered collection 
+of unique items.
+
+Features:
+
+In set we can see different Output 
+In set every element is unique element
+In set there is unorder index
+
+*/
+
+void main() {
+  var s = {'hello', '1', '2'};
+  print(s);
+}
