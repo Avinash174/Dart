2e5ddc1ab281a4421585239cc934f8cc013bca50
Avinash Magar <42104652+Avinash174@users.noreply.github.com>
2024-11-29 09:19:31 +0530

    set part 2

--- Dart100_Days/set.dart
+++ Dart100_Days/set.dart
@@ -11,9 +11,17 @@ In set we can see different Output
 In set every element is unique element
 In set there is unorder index
 
+when we declare using var it become Map i.e vae a={}
+when we declare as set it become Set i.e Set s1=new Set()
+
 */
 
 void main() {
   var s = {'hello', '1', '2'};
   print(s);
+
+  Set s1 = new Set();
+  s1.add(10);
+  print(s1);
+  print(s1.runtimeType);
 }
