c9b251b8922448ec4d8fe85216a268ed0425548e
Avinash Magar <avinashmagar15@gmail.com>
2024-12-08 21:19:22 +0530

    try catch exeption

--- Dart100_Days/exeption_handlin.dart
+++ Dart100_Days/exeption_handlin.dart
@@ -3,6 +3,26 @@
 /* 
 The execution of program is terminated 
 abnormorly
+
+1. DefferLoadExecption 
+
+Thrown when a deferred library fails to load
+
+2. Format Exeception
+
+The exeption is thrown when a string or 
+some other data does not have an expected 
+format and cannot be processed.
+
+3.IntegerDivisionByZero
+
+The execution is thrown when a number 
+is divisible by zero
+
+4. IO Exeception
+
+Base class for all Input-Output 
+related execption
 */
 
 void main() {
