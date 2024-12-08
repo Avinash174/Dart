0dbcfcf31ac855a4709299ca92ee58d586ffb0ce
Avinash Magar <avinashmagar15@gmail.com>
2024-12-08 22:10:47 +0530

    new code changes

--- Dart100_Days/catch_clause.dart
+++ Dart100_Days/catch_clause.dart
@@ -1,2 +1,13 @@
 // using catch clause exeption handling.
 
+/* 
+When we don't known about the
+exeption then use the catch clause
+
+You can specify one or two parameters to
+catch(). the first is the exception object
+that was thrown, and the second is the 
+stack Trace object.
+*/
+
+void main() {}
