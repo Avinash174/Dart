669b5d98d4f1c8f4e93c1e22bd519900272c96d0
Avinash Magar <avinashmagar15@gmail.com>
2024-12-09 09:46:30 +0530

    new code

---
+++ Dart100_Days/oop_concept/class_boject.dart
@@ -0,0 +1,20 @@
+// class object 
+
+/*
+ A class is blueprint for an objects.
+
+ You can create an object using
+ a constructor.
+ */
+
+/* 
+class class_name {
+ class_member
+}
+
+var object_name=new ClassName();
+
+New keyword is optional
+
+*/ 
+
