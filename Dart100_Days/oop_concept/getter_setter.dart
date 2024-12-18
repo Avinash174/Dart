b099d2641dce1cf6a2c870d1c6ebfe79cb1ba94c
Avinash Magar <avinashmagar15@gmail.com>
2024-12-18 09:55:15 +0530

    getter and setter

--- Dart100_Days/oop_concept/getter_setter.dart
+++ Dart100_Days/oop_concept/getter_setter.dart
@@ -5,4 +5,33 @@ Getter and Setter are specficial methods
 that provide read and write access to an 
 onject's properties.
 
+Recall that each instance variable has 
+an implicit getter,plus a setter if
+appropriate
+
 */
+
+void main() {
+  var a = A();
+  a.x = 10; // Default setter
+  print(a.x); // Default getter
+
+  a.cutSet = 'Avinash';
+  print(a.cutSet);
+}
+
+class A {
+  var x;
+
+  var name;
+
+  //custom getter
+
+  void set cutSet(var name) {
+    this.name = name;
+  }
+
+  String get cutSet {
+    return name;
+  }
+}
