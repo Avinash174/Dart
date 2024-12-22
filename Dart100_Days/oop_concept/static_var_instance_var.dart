dc901d9a1bc213fae44ce41df39f269722d24315
Avinash Magar <avinashmagar15@gmail.com>
2024-12-22 11:32:06 +0530

    new changes

---
+++ Dart100_Days/oop_concept/static_var_instance_var.dart
@@ -0,0 +1,12 @@
+/*
+
+Static varible vs Instance Varible
+
+Instance varible & Instance Methods is 
+based on the project.
+
+Static varible or class variable & static 
+methods is based on the class
+
+
+*/
