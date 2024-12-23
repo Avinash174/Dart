4f9fc4ab3b72641c3e290dc96330a9c01579aca5
Avinash Magar <avinashmagar15@gmail.com>
2024-12-23 08:54:46 +0530

    static variable & instance varible

--- Dart100_Days/oop_concept/static_var_instance_var.dart
+++ Dart100_Days/oop_concept/static_var_instance_var.dart
@@ -10,3 +10,17 @@ methods is based on the class
 
 
 */
+
+void main() {
+  var a = A();
+  print(a.x);
+  print(a.x = 30);
+  print(a.x = 20);
+  print(A.y);
+}
+
+class A {
+  int x = 10;
+
+  static int y = 50;
+}
