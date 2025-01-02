941c07b64c908a03f6a6ffc141d53f5e1bc85d1a
Avinash Magar <avinashmagar15@gmail.com>
2025-01-02 09:23:48 +0530

    generic class

--- Dart100_Days/oop_concept/generic.dart
+++ Dart100_Days/oop_concept/generic.dart
@@ -1,4 +1,19 @@
 /* 
 Generic class 
 
+The use of generic makes type-safe
+collection.
+
+Generic are often required for type 
+safety,but they have benefits than 
+just allowing your code to run :
+
+Properly specific generic types result
+in better-generated code.
+
+You can use generic to reduce code 
+duplication
+
 */
+
+void main() {}
