efd2cdfcceb9521bcb2e68a3efc6f475a3581432
Avinash Magar <avinashmagar15@gmail.com>
2025-01-02 09:34:42 +0530

    null safety

--- Dart100_Days/oop_concept/nullsafety.dart
+++ Dart100_Days/oop_concept/nullsafety.dart
@@ -1,3 +1,22 @@
+/* 
+Dart 2.12 Introduced null 
+safety meaning that values
+can't be null unless you 
+say they can be. In other 
+words types are non-nullable
+by default.
+
+null safety makes our code 
+safer and run faster
+
+two operator used in null safety 
+? !
+*/
+
 void main() {
-  
+  int? x;
+  int y = x!;
+
+  print(x);
+  print(y);
 }
