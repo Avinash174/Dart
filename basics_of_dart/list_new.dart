8f13a4ba3dc64a14390007afda28bfd60b59c089
Avinash175 <avinashmagar15@gmail.com>
2026-03-13 22:55:22 +0530

    new code

---
+++ basics_of_dart/list_new.dart
@@ -0,0 +1,8 @@
+import 'dart:ffi';
+
+void main() {
+  List<int> numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  print("Integer list is : $numbers");
+  List<String> names = ["Alice", "Bob", "Charlie"];
+  print("String list is : $names");
+}
